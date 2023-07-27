efcff0c322fd32b9b6770555227ff2255071b49b
Cong Van <congvan2405@gmail.com>
2023-07-27 14:31:27 +0700

    Update query

--- prj321x_asm01.sql
+++ prj321x_asm01.sql
@@ -5,12 +5,12 @@ create database prj321x_asm01;
 use prj321x_asm01;
 
 create table `role`(
-	id int(11) not null auto_increment primary key,
+	id int not null auto_increment primary key,
     role_name varchar(255)
 );
 
 create table `user`(
-	id int(11) not null auto_increment primary key,
+	id int not null auto_increment primary key,
     address varchar(255) not null,
     email varchar(255) not null,
     full_name varchar(255) not null,
@@ -20,33 +20,33 @@ create table `user`(
     `status` int not null,
     username varchar(255) not null,
     created datetime not null default now(),
-    role_id int(11) not null,
+    role_id int not null,
     foreign key(role_id) references `role`(id)
 );
 
 create table donation(
-	id int(11) not null auto_increment primary key,
+	id int not null auto_increment primary key,
     `code` varchar(255) not null,
     created datetime not null default now(),
     `description` varchar(255) not null,
     start_date date not null,
     end_date date not null,
     `name` varchar(255) not null,
-    money int(11) not null default 0,
+    money int not null default 0,
     organization_name varchar(255) not null,
     phone_number varchar(255) not null,
-    `status` int(11) not null
+    `status` int not null
 );
 
 create table user_donation(
-	id int(11) not null auto_increment primary key,
+	id int not null auto_increment primary key,
     created date not null default (curdate()),
-    money int(11) not null,
+    money int not null,
     `name` varchar(255) not null,
-    `status` int(11) not null,
+    `status` int not null,
     `text` varchar(255),
-    donation_id int(11),
-    user_id int(11),
+    donation_id int,
+    user_id int,
     foreign key(donation_id) references donation(id) on delete cascade,
     foreign key(user_id) references user(id) on delete cascade
 );
