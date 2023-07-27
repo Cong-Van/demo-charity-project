074b841290fd64e79f34ae78fe85c2598b672b69
Cong Van <congvan2405@gmail.com>
2023-07-27 16:09:26 +0700

    Update accordingly

--- prj321x_asm01.sql
+++ prj321x_asm01.sql
@@ -4,12 +4,12 @@ create database prj321x_asm01;
 
 use prj321x_asm01;
 
-create table `role`(
+create table roles (
 	id int not null auto_increment primary key,
     role_name varchar(255)
 );
 
-create table `user`(
+create table users (
 	id int not null auto_increment primary key,
     address varchar(255) not null,
     email varchar(255) not null,
@@ -21,10 +21,10 @@ create table `user`(
     username varchar(255) not null,
     created datetime not null default now(),
     role_id int not null,
-    foreign key(role_id) references `role`(id)
+    foreign key(role_id) references roles (id)
 );
 
-create table donation(
+create table donations (
 	id int not null auto_increment primary key,
     `code` varchar(255) not null,
     created datetime not null default now(),
@@ -38,7 +38,7 @@ create table donation(
     `status` int not null
 );
 
-create table user_donation(
+create table user_donations (
 	id int not null auto_increment primary key,
     created date not null default (curdate()),
     money int not null,
@@ -47,23 +47,23 @@ create table user_donation(
     `text` varchar(255),
     donation_id int,
     user_id int,
-    foreign key(donation_id) references donation(id) on delete cascade,
-    foreign key(user_id) references user(id) on delete cascade
+    foreign key(donation_id) references donations (id) on delete cascade,
+    foreign key(user_id) references users (id) on delete cascade
 );
 
-insert into role values(1, "ADMIN"), (2, "USER");
+insert into roles values(1, "ADMIN"), (2, "USER");
 
-insert into `user`(address, email, full_name, `password`, phone_number,
+insert into users (address, email, full_name, `password`, phone_number,
 				`status`, username, role_id)
-			values("12 Quang Trung", "admin@gmail.com", "Nguyễn Huy", "113", "0325164899",
+			values ("12 Quang Trung", "admin@gmail.com", "Nguyễn Huy", "113", "0325164899",
 					1, "huynguyen", 1),
                   ("21 Nguyễn Huệ", "ngoc@gmail.com", "Minh Ngọc", "115", "0328464852",
 					0, "minhngoc", 2);
                     
-insert into donation(`code`, `description`, start_date, end_date,
+insert into donations (`code`, `description`, start_date, end_date,
 					`name`, organization_name, phone_number, `status`)
 			values("IDE113", "Ủng hộ trẻ em miền núi", "2023-06-06",
 					"2023-06-09", "Hỗ trợ trẻ em miền núi", "Hội từ thiện", "0345671148", 1);
                     
-insert into user_donation(money, `name`, `status`, `text`, donation_id, user_id)
+insert into user_donations (money, `name`, `status`, `text`, donation_id, user_id)
 			values(1000000, "ngoc", 1, "Goodluck", 1, 2);
